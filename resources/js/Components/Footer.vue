ab8d895969c7881a861361b2e7c7d28e1f1aa36f
izzy010101 <165940159+izzy010101@users.noreply.github.com>
2025-05-02 08:55:23 -0500

    adding links to footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -98,23 +98,29 @@ const submit = async () => {
         <!-- Bottom -->
         <div class="mt-10 border-t border-gray-700 pt-6 text-center text-sm text-gray-400 dark:text-gray-500">
             <div class="flex justify-center gap-6 mb-4">
-                <a href="#" class="hover:text-white dark:hover:text-green-300" aria-label="Facebook">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M22 12a10 10 0 1 0-11.57 9.87v-7H8v-2.9h2.43v-2.2c0-2.4 1.43-3.73 3.63-3.73 1.05 0 2.15.19 2.15.19v2.36h-1.21c-1.2 0-1.57.75-1.57 1.52v1.86H18l-.4 2.9h-2.62v7A10 10 0 0 0 22 12z" />
+                <!-- Instagram -->
+                <a href="https://www.instagram.com/echo_mockingbird/" target="_blank" class="hover:text-white dark:hover:text-green-300" aria-label="Instagram">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 2H17C20 2 22 4 22 7V17C22 20 20 22 17 22H7C4 22 2 20 2 17V7C2 4 4 2 7 2ZM15.5 11.5C15.5 13.9853 13.4853 16 11 16C8.51472 16 6.5 13.9853 6.5 11.5C6.5 9.01472 8.51472 7 11 7C13.4853 7 15.5 9.01472 15.5 11.5ZM17 7.5C17.2761 7.5 17.5 7.72386 17.5 8C17.5 8.27614 17.2761 8.5 17 8.5C16.7239 8.5 16.5 8.27614 16.5 8C16.5 7.72386 16.7239 7.5 17 7.5Z" />
                     </svg>
                 </a>
-                <a href="#" class="hover:text-white dark:hover:text-green-300" aria-label="Twitter">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M22.46 6c-.77.35-1.6.59-2.46.7a4.29 4.29 0 0 0 1.88-2.37 8.62 8.62 0 0 1-2.72 1.04A4.28 4.28 0 0 0 16.1 4c-2.37 0-4.29 1.92-4.29 4.29 0 .34.04.67.11.99A12.19 12.19 0 0 1 3 5.16a4.29 4.29 0 0 0 1.32 5.72 4.27 4.27 0 0 1-1.94-.54v.05a4.3 4.3 0 0 0 3.44 4.2 4.36 4.36 0 0 1-1.93.07 4.29 4.29 0 0 0 4 2.98 8.61 8.61 0 0 1-6.32 1.76 12.1 12.1 0 0 0 6.56 1.92c7.88 0 12.2-6.53 12.2-12.2 0-.19 0-.38-.01-.57A8.6 8.6 0 0 0 24 5.34a8.42 8.42 0 0 1-2.54.7z" />
+
+                <!-- YouTube -->
+                <a href="https://www.youtube.com/@zezobraznica343" target="_blank" class="hover:text-white dark:hover:text-green-300" aria-label="YouTube">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 15L15 12L10 9V15ZM22 12C22 9.8 21.8 8.2 21.7 7.4C21.6 6.6 21 6 20.2 5.9C19.4 5.8 12 5.8 12 5.8C12 5.8 4.6 5.8 3.8 5.9C3 6 2.4 6.6 2.3 7.4C2.2 8.2 2 9.8 2 12C2 14.2 2.2 15.8 2.3 16.6C2.4 17.4 3 18 3.8 18.1C4.6 18.2 12 18.2 12 18.2C12 18.2 19.4 18.2 20.2 18.1C21 18 21.6 17.4 21.7 16.6C21.8 15.8 22 14.2 22 12Z" />
                     </svg>
                 </a>
-                <a href="#" class="hover:text-white dark:hover:text-green-300" aria-label="Instagram">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M12 2.16c3.18 0 3.56.01 4.81.07 1.2.06 1.96.25 2.41.42a4.79 4.79 0 0 1 1.75 1.14 4.79 4.79 0 0 1 1.14 1.75c.17.45.36 1.2.42 2.41.06 1.25.07 1.63.07 4.81s-.01 3.56-.07 4.81c-.06 1.2-.25 1.96-.42 2.41a4.79 4.79 0 0 1-1.14 1.75 4.79 4.79 0 0 1-1.75 1.14c-.45.17-1.2.36-2.41.42-1.25.06-1.63.07-4.81.07s-3.56-.01-4.81-.07c-1.2-.06-1.96-.25-2.41-.42a4.79 4.79 0 0 1-1.75-1.14 4.79 4.79 0 0 1-1.14-1.75c-.17-.45-.36-1.2-.42-2.41C2.17 15.56 2.16 15.18 2.16 12s.01-3.56.07-4.81c.06-1.2.25-1.96.42-2.41A4.79 4.79 0 0 1 3.79 3.03a4.79 4.79 0 0 1 1.75-1.14c.45-.17 1.2-.36 2.41-.42C8.44 2.17 8.82 2.16 12 2.16Zm0 3.5a5.34 5.34 0 1 1 0 10.67A5.34 5.34 0 0 1 12 7.26Zm0 1.6a3.74 3.74 0 1 0 0 7.47 3.74 3.74 0 0 0 0-7.47Zm5.47-1.47a1.25 1.25 0 1 1-2.5 0 1.25 1.25 0 0 1 2.5 0Z" />
+
+                <!-- Placeholder (use star icon for now) -->
+                <a href="#" class="hover:text-white dark:hover:text-green-300" aria-label="Coming Soon">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 dark:text-gray-400 hover:text-white dark:hover:text-green-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.562 4.804a1 1 0 00.95.69h5.043c.969 0 1.371 1.24.588 1.81l-4.082 2.962a1 1 0 00-.364 1.118l1.562 4.804c.3.921-.755 1.688-1.54 1.118l-4.082-2.962a1 1 0 00-1.176 0l-4.082 2.962c-.784.57-1.838-.197-1.54-1.118l1.562-4.804a1 1 0 00-.364-1.118L2.404 10.23c-.783-.57-.38-1.81.588-1.81h5.043a1 1 0 00.95-.69l1.562-4.804z" />
                     </svg>
                 </a>
             </div>
-            © 2025 FuturaFly. All rights reserved.
         </div>
+
+
     </footer>
 </template>
